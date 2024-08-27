d1f572e8b5d5ff0536bd6b419925230e34f81200
Surabhi Nair <surabhinair1914@gmail.com>
2024-08-27 15:36:14 +0000

    First level changes performed

---
+++ HW1.py
@@ -0,0 +1,18 @@
+# Initializing the variable 'r' to denote the radius of a circle
+r=26
+pie=3.142
+
+#Finding the circumference of a circle
+c= 2*pie*r
+#Finding the Diameter of a circle
+d=2*r
+#Finding the Area of a circle
+A=pie*(r*r)
+
+#Display the circumference, diameter and area of a circle
+pri("Radius of a circle is ",r)
+print("Area of a circle is ",A)
+print("Diameter of a circle is ",d)
+print("Circumference of a circle is ",c)
+
+
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # HW1
+![Static Badge](https://img.shields.io/badge/Python-Black?style=for-the-badge&logo=Python)
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
