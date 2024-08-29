ea976efb3765ea276d2f576b2545dc797c817f3e
Surabhi Nair <surabhinair1914@gmail.com>
2024-08-29 00:01:25 +0000

    Completed 10th point

--- HW1.py
+++ HW1.py
@@ -1,18 +1,21 @@
-# Initializing the variable 'r' to denote the radius of a circle
-r=26
-pie=3.142
+# defining diameter function
+def diameter(r):
+    return 2*r
 
-#Finding the circumference of a circle
-c= 2*pie*r
 #Finding the Diameter of a circle
-d=2*r
-#Finding the Area of a circle
-A=pie*(r*r)
+d=diameter(20)
 
-#Display the circumference, diameter and area of a circle
-pri("Radius of a circle is ",r)
+# defining area function
+def area(r):
+    pie=3.142
+    return 2*pie*r
+
+#Finding the Area of a circle
+A=area(20)
+
+#Display the diameter and area of a circle
 print("Area of a circle is ",A)
 print("Diameter of a circle is ",d)
-print("Circumference of a circle is ",c)
+
 
 
---
+++ HW1_test.py
@@ -0,0 +1,10 @@
+import HW1
+
+#Failed case
+def test_success_diameter():
+   assert HW1.diameter(10) == 20
+
+#Success file
+def test_failure_area():
+   assert HW1.area(10) == 20
+
