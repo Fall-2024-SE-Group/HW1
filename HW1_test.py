83a04aca088ab39d663fac81829bd075fe1a663c
Sreeja-Nukarapu <sreeja.nukarapu2263@gmail.com>
2024-08-28 20:03:35 -0400

    Update HW1_test.py

--- HW1_test.py
+++ HW1_test.py
@@ -1,10 +1,10 @@
 import HW1
 
-#Failed case
+#Success file
 def test_success_diameter():
    assert HW1.diameter(10) == 20
 
-#Success file
+#Failed case
 def test_failure_area():
    assert HW1.area(10) == 20
 
